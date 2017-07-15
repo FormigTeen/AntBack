7787fd67d2cb66e4ab2f47c9ea7e54f220635e50
Matheus Silva Freitas <msformigteen@live.com>
2017-07-15 20:14:22 -0300

    Composer Instalado

--- AntApp/Caller/AntPath.php
+++ AntApp/Caller/AntPath.php
@@ -1,5 +1,8 @@
 <?php
 
+//Chama o Composer
+require_once ( __DIR__ .'/../vendor/autoload.php');
+
 //Ativa o Gatilho do Ant
 require_once( __DIR__ . '/AntSign.php');
 
---
+++ AntApp/Validation/Exception/Log/AntLogger.php
@@ -0,0 +1,5 @@
+<?php
+
+  namespace AntBack\Validation\Exception\Log;
+
+  class AntLog 
