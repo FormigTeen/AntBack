4e392f455d49b2625ac6f7f9aabe957d9bd3c10f
Matheus Silva Freitas <msformigteen@live.com>
2017-07-21 17:13:10 -0300

    Padrao Modificado no Database

--- AntApp/DataBase/AntBridge.php
+++ AntApp/DataBase/AntBridge.php
@@ -11,18 +11,23 @@
   //Conecta o todos os Módulos Secundarios de AntBridge
   require_once( __DIR__ . '/Extern/Connection.php');
 
-  class AntBridge {
+  class AntBridge
+  {
 
     private $Table;
     private $Action;
     private $Stack[];
     private $Condition[];
 
-    final public function setBridge() {
+    //Decide qual é o Banco de Dados que deverá ser utilizado
+    final public function setBridge()
+    {
     }
 
-    final public function setTable(string $Table) {
-      $this->Table();
+    //Informa qual é a Tablea que será utilizada para construir a Query
+    final public function setTable(string $Table)
+    {
+      $this->Table = $Table;
     }
 
   }
--- AntApp/DataBase/Sintaxe/MySQL.php
+++ AntApp/DataBase/Sintaxe/MySQL.php
@@ -4,7 +4,8 @@
 
   use DataBASE\Sintaxe\MySQL as MySQL;
 
-  class MySQL {
+  class MySQL
+  {
 
     private $Query;
     private $Table;
@@ -12,40 +13,46 @@
     private $NumStack;
     private $Condition;
 
-    final public function __construct($table, $field = array(), $numStack ) {
+    final public function __construct($table, $field = array(), $numStack )
+    {
       $this->Table = $table;
       $this->Field = $field;
       $this->NumStack = $numStack;
       $this->Insert();
     }
 
-    final public function Insert() {
+    final public function Insert()
+    {
       $this->InsertLevelOne();
       $this->InsertLevelTwo();
       var_dump($this->Query);die;
     }
 
-    final private function InsertLevelOne() {
+    final private function InsertLevelOne()
+    {
       $this->Query = "INSERT INTO `" . $this->Table . "` (" . implode(", ", $this->Field) . ") VALUES ";
     }
 
-    final private function Select() {
+    final private function Select()
+    {
 
     }
 
-    final private function InsertLevelTwo() {
+    final private function InsertLevelTwo()
+    {
       $rowPlaces = '(' . implode(', ', array_fill(0, count($this->Field), '?')) . ')';
       $this->Query .= implode(', ', array_fill(0, $this->NumStack, $rowPlaces));
     }
 
-    final private function startUpdate() {
+    final private function startUpdate()
+    {
 
     }
 
-    final private function startDelete() {
+    final private function startDelete()
+    {
 
     }
-
 
   }
 
