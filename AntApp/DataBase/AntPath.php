b25e33da639d979a6a5d0e7f9734ebc44e0580b2
Matheus Silva Freitas <msformigteen@live.com>
2017-07-22 08:57:09 -0300

    Query de Select e Multiplo Select Criada

---
+++ AntApp/DataBase/AntPath.php
@@ -0,0 +1,8 @@
+<?php
+
+  //Conecta todas as Sintaxes
+  require_once( __DIR__ . '/Sintaxe/MySQL.php');
+  
+''//Conecta o todos os Módulos Secundarios de AntBridge
+  require_once( __DIR__ . '/Extern/Connection.php');
+  require_once( __DIR__ . '/AntBridge.php');
--- AntApp/DataBase/Sintaxe/MySQL.php
+++ AntApp/DataBase/Sintaxe/MySQL.php
@@ -17,15 +17,15 @@
     {
       $this->Table = $table;
       $this->Field = $field;
-      $this->NumStack = $numStack;
-      $this->Insert();
+      $this->Condition = $numStack;
+      $this->Select(); //DEBUG
+      var_dump($this->Query);die;
     }
 
     final public function Insert()
     {
       $this->InsertLevelOne();
       $this->InsertLevelTwo();
-      var_dump($this->Query);die;
     }
 
     final private function InsertLevelOne()
@@ -33,15 +33,40 @@
       $this->Query = "INSERT INTO `" . $this->Table . "` (" . implode(", ", $this->Field) . ") VALUES ";
     }
 
-    final private function Select()
-    {
-
-    }
-
     final private function InsertLevelTwo()
     {
       $rowPlaces = '(' . implode(', ', array_fill(0, count($this->Field), '?')) . ')';
       $this->Query .= implode(', ', array_fill(0, $this->NumStack, $rowPlaces));
+    }
+
+    final public function Select()
+    {
+      $this->SelectLevelOne();
+    }
+
+    final private function SelectLevelOne()
+    {
+
+      if ( is_array($this->Table) ) {
+        $this->Query = $this->SelectLevelThree();
+      } else {
+        $this->Query = $this->SelectLevelTwo($this->Table);
+      }
+    }
+
+    final private function SelectLevelTwo( string $table )
+    {
+      return "SELECT * FROM `{$table}` " . $this->Condition ?? ' ';
+    }
+
+    final private function SelectLevelThree() {
+
+      $map = [];
+      foreach ($this->Table as $Table ) {
+        $map[] = "( {$this->SelectLevelTwo($Table)} )" . " AS {$Table}";
+      }
+
+      return "SELECT " . implode(", ", $map);
     }
 
     final private function startUpdate()
@@ -56,4 +81,4 @@
 
   }
 
-  new MySQL( "users", ['name', 'age', 'andress'], 8);
+  new MySQL( ['users', 'jobs'], ['name', 'age', 'andress'], "WHERE `id` = 5");
